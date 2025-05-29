08cbe2caf0c4bb9dba48b5e0f9c1b82552303a58
Salvatore Di Gennaro <salvatore.digennaro@outlook.it>
2025-05-29 10:06:12 +0200

    Size fixes

--- scripts/single_process_benchmark.sh
+++ scripts/single_process_benchmark.sh
@@ -9,10 +9,10 @@ benchmark() {
     echo -n "100;100;100;" >> single_process_results.csv && ./bin/single_process_benchmark -r 1 -c 1 -w 32 -x 1 -y 1 -m 100 -k 100 -n 100 | sed 's/\s\+/;/' >> single_process_results.csv
 
     echo "100x1000 1000x100"
-    echo -n "1000;1000;1000;" >> single_process_results.csv && ./bin/single_process_benchmark -r 1 -c 1 -w 32 -x 1 -y 1 -m 1000 -k 1000 -n 1000 | sed 's/\s\+/;/' >> single_process_results.csv
+    echo -n "100;1000;100;" >> single_process_results.csv && ./bin/single_process_benchmark -r 1 -c 1 -w 32 -x 1 -y 1 -m 100 -k 1000 -n 100 | sed 's/\s\+/;/' >> single_process_results.csv
 
     echo "100x10000 10000x100"
-    echo -n "1000;10000;1000;" >> single_process_results.csv && ./bin/single_process_benchmark -r 1 -c 1 -w 32 -x 1 -y 1 -m 1000 -k 10000 -n 1000 | sed 's/\s\+/;/' >> single_process_results.csv
+    echo -n "100;10000;100;" >> single_process_results.csv && ./bin/single_process_benchmark -r 1 -c 1 -w 32 -x 1 -y 1 -m 100 -k 10000 -n 100 | sed 's/\s\+/;/' >> single_process_results.csv
 
     # echo "100x100000 100000x100"
     # echo -n "1000;100000;1000;$1;" >> single_process_results.csv && ./bin/single_process_benchmark -r 1 -c 1 -w 32 -x 1 -y 1 -m 1000 -k 100000 -n 1000 >> single_process_results.csv
