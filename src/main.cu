2c394594f06f0df24054ce5f15a70d54105681ae
Redy1908 <58226371+Redy1908@users.noreply.github.com>
2025-06-01 18:49:23 +0200

    Updated

--- src/main.cu
+++ src/main.cu
@@ -114,7 +114,6 @@ int main(int argc, char *argv[]) {
 
   // test di efficienza al crescere delle dimensioni della metrice ed il numero di thread
   for (Nglob = 2048; Nglob <= 2048 * 3; Nglob = Nglob + 2048) {
-
     /*
      * Test con 1 thread, bisogna considerare che questo test sarà essere molto lento, attualmente lo script limita l'esecuzione a 5 minuti
      * consiglio di rimuove questo test per testare il codice
@@ -131,13 +130,28 @@ int main(int argc, char *argv[]) {
     phpc_gemm_summa_cuda(grid_comm, A, B, C, ld, ld, ld, Nglob, Nglob, Nglob, dim_block, dim_grid, shared_mem_size);
     end_time = get_cur_time() - start_time;
 
-    // test con 1024 thread
+    /*
+     * Test con 1024 thread per blocco (GPU LIMIT) e grid size ottimale
+     *
+     * Questo test dovrebbe darci il risultato migliore considerando una matrice abbastanza grande
+     *
+     * La dimensione della griglia viene calcolata automaticamente in modo ottimale dentro summa nel
+     * seguente modo: dim_grid.x = (unsigned int)ceil((double)local_B_cols / dim_block.x);
+     *                grid.y = (unsigned int)ceil((double)local_A_rows / dim_block.y);
+     *
+     * considerando le dimensioni effettive delle porzioni di matrici che verranno moltiplicate.)
+     *
+     * Ad esempio: consideriamo 2 blocchi di matrici 2048x2048, con tile_width = 32, avremo una griglia di 64x64 blocchi
+     * da 1024 thread ciascuno, quindi 64*64*1024 = 4194304 thread. Questo ci permette di mappare 1:1 i blocchi di matrice
+     * con i thread della GPU, e di avere il massimo parallelismo possibile.
+     *
+     */
     MPI_Barrier(MPI_COMM_WORLD);
-    tile_width = 32;
-    check_threads_per_block(prop, tile_width, rank);
-    check_shared_memory_usage(prop, tile_width, rank);
+    tile_width = 32;                                    // block size 32x32 = 1024 threads
+    check_threads_per_block(prop, tile_width, rank);    // controlliamo lo stesso di non superare i 1024 threads
+    check_shared_memory_usage(prop, tile_width, rank);  // controlliamo di avere abbastanza shared memory per copiarci 2 tile 32x32 di double
     dim_block = dim3(tile_width, tile_width, 1);
-    dim_grid = dim3(0, 0, 0);  // quando passiamo dim3(0,0,0) la dmensione della griglia viene calcolata automaticamente in modo ottimale dentro summa
+    dim_grid = dim3(0, 0, 0);  // quando passiamo dim3(0,0,0) la dimensione della griglia viene calcolata automaticamente in modo ottimale dentro summa
     shared_mem_size = 2 * tile_width * tile_width * sizeof(double);
 
     start_time = get_cur_time();
