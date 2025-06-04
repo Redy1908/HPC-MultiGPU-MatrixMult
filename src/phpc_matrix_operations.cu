90bed525bfcb057864e0fdce3893dd9865a85e1e
Salvatore Di Gennaro <salvatore.digennaro@outlook.it>
2025-06-04 16:40:24 +0200

    Working version without gather

--- src/phpc_matrix_operations.cu
+++ src/phpc_matrix_operations.cu
@@ -7,6 +7,8 @@
 #include "phpc_matrix_operations.cuh"
 #include "utils.cuh"
 
+#define IDX(row, col, num_cols) ((row) * (num_cols) + (col))
+
 __global__ void gemm_kernel(double *A, double *B, double *C, int M, int N, int K) {
   extern __shared__ double shared_mem[];
 
@@ -65,25 +67,27 @@ __global__ void gemm_kernel(double *A, double *B, double *C, int M, int N, int K
   }
 }
 
-int phpc_gemm_cuda(const double *a, int lda, const double *b, int ldb, double *c, int ldc, int width, int gpu_count, int grid_width, int grid_height, int block_width) {
+int phpc_gemm_sequential(const double *A, int lda, const double *B, int ldb, double *C, int ldc, unsigned int m, unsigned int k, unsigned int n) {
+  for (unsigned int i = 0; i < m; ++i)
+    for (unsigned int j = 0; j < n; ++j)
+      for (unsigned int l = 0; l < k; ++l)
+        C[IDX(i, j, ldc)] += A[IDX(i, l, lda)] * B[IDX(l, j, ldb)];
+
+  return 0;
+}
+
+int phpc_gemm_cuda(const double *a, int lda, const double *b, int ldb, double *c, int ldc, int m, int k, int n, int gpu_count, int grid_width, int grid_height, int block_width) {
   int device_count;
   cudaGetDeviceCount(&device_count);
 
-  assert(width > 0);
+  // assert(matrices_width > 0);
   assert(gpu_count > 0 && gpu_count <= device_count);
   assert(grid_width > 0 && grid_height > 0);
   assert(block_width * block_width <= 1024);
-  assert(width % gpu_count == 0);
+  assert(n % gpu_count == 0);
 
   /**
-   * Matrix A: each gpu has a "row"
-   *  ________________________
-   * |         GPU 0         |
-   * -------------------------
-   * |          ...          |
-   * -------------------------
-   * |         GPU N         |
-   * -------------------------
+   * Matrix A: each gpu copies the entire matrix
    *
    * Matrix B: each gpu has a "column"
    *  ________________________
@@ -93,10 +97,18 @@ int phpc_gemm_cuda(const double *a, int lda, const double *b, int ldb, double *c
    * |       |       |       |
    * |       |       |       |
    * -------------------------
+   *
+   * Matrix C: each gpu has a resulting "column"
+   *  ________________________
+   * |       |       |       |
+   * |       |       |       |
+   * | GPU 0 |  ...  | GPU i |
+   * |       |       |       |
+   * |       |       |       |
+   * -------------------------
    */
 
-  int m = width / gpu_count;
-  int k = width;
+  int dev_n = n / gpu_count;
 
   double **dev_buffers_a = (double **)malloc(gpu_count * sizeof(double *));
   double **dev_buffers_b = (double **)malloc(gpu_count * sizeof(double *));
@@ -108,55 +120,41 @@ int phpc_gemm_cuda(const double *a, int lda, const double *b, int ldb, double *c
   assert(dev_buffers_c != NULL);
   assert(streams != NULL);
 
-  for (int i = 0; i < gpu_count; i++) {
-    cudaSetDevice(i);
-    cudaMalloc(&(dev_buffers_a[i]), m * k * sizeof(double));
-    cudaMalloc(&(dev_buffers_b[i]), m * k * sizeof(double));
-    cudaMalloc(&(dev_buffers_c[i]), m * k * sizeof(double));
-    cudaStreamCreate(&(streams[i]));
+  for (int gpu = 0; gpu < gpu_count; gpu++) {
+    cudaSetDevice(gpu);
+    cudaMalloc(&(dev_buffers_a[gpu]), m * k * sizeof(double));
+    cudaMalloc(&(dev_buffers_b[gpu]), k * dev_n * sizeof(double));
+    cudaMalloc(&(dev_buffers_c[gpu]), k * dev_n * sizeof(double));
+    cudaStreamCreate(&(streams[gpu]));
 
-    /* copy column of B*/
-    cudaMemcpy2DAsync(dev_buffers_b[i], m * sizeof(double), b + m * i, ldb * sizeof(double), m * sizeof(double), width, cudaMemcpyHostToDevice, streams[i]);
+    /* TODO: assert cudaMalloc success */
 
-    /* copy column of C */
-    cudaMemcpy2DAsync(dev_buffers_c[i], m * sizeof(double), c + m * i, ldc * sizeof(double), m * sizeof(double), width, cudaMemcpyHostToDevice, streams[i]);
+    cudaMemcpy2DAsync(dev_buffers_a[gpu], k * sizeof(double), a, lda * sizeof(double), k * sizeof(double), m, cudaMemcpyHostToDevice, streams[gpu]);
+    cudaMemcpy2DAsync(dev_buffers_b[gpu], dev_n * sizeof(double), b + dev_n * gpu, ldb * sizeof(double), dev_n * sizeof(double), k, cudaMemcpyHostToDevice, streams[gpu]);
+    cudaMemcpy2DAsync(dev_buffers_c[gpu], dev_n * sizeof(double), c + dev_n * gpu, ldc * sizeof(double), dev_n * sizeof(double), k, cudaMemcpyHostToDevice, streams[gpu]);
   }
 
   dim3 grid_size(grid_width, grid_height, 1);
   dim3 block_size(block_width, block_width, 1);
   int shared_memory_size = 2 * block_width * block_width * sizeof(double);
 
-  for (int i = 0; i < gpu_count; i++) {
-    for (size_t j = 0; j < gpu_count; j++) {
-      cudaSetDevice(j);
+  for (int gpu = 0; gpu < gpu_count; gpu++) {
+    cudaSetDevice(gpu);
 
-      /* copy row j % gpu_count of A */
-      int row = (j + i) % gpu_count;
-      cudaMemcpyAsync(dev_buffers_a[j], a + m * k * row, m * k * sizeof(double), cudaMemcpyHostToDevice, streams[j]);
+    /* perform computation */
+    gemm_kernel<<<grid_size, block_size, shared_memory_size, streams[gpu]>>>(dev_buffers_a[gpu], dev_buffers_b[gpu], dev_buffers_c[gpu] + dev_n * gpu, m, k, dev_n);
 
-      int dev_offset_c = m * m * row;
-      gemm_kernel<<<grid_size, block_size, shared_memory_size, streams[j]>>>(dev_buffers_a[j], dev_buffers_b[j], dev_buffers_c[j] + dev_offset_c, m, m, k);
-    }
+    /* copy result from device to host */
+    cudaMemcpy2DAsync(c + dev_n * gpu, ldc * sizeof(double), dev_buffers_c[gpu], dev_n * sizeof(double), dev_n * sizeof(double), m, cudaMemcpyDeviceToHost, streams[gpu]);
   }
 
-  /**
-   * Gather results: each GPU has a column of the resulting matrix C
-   *  ________________________
-   * |       |       |       |
-   * |       |       |       |
-   * | GPU 0 |  ...  | GPU i |
-   * |       |       |       |
-   * |       |       |       |
-   * -------------------------
-   */
-  for (int i = 0; i < gpu_count; i++) {
-    cudaSetDevice(i);
-    cudaMemcpy2D(c + m * i, width * sizeof(double), dev_buffers_c[i], m * sizeof(double), m * sizeof(double), width, cudaMemcpyDeviceToHost);
-
-    cudaFree(&(dev_buffers_c[i]));
-    cudaFree(&(dev_buffers_b[i]));
-    cudaFree(&(dev_buffers_a[i]));
-    cudaStreamDestroy(streams[i]);
+  for (int gpu = 0; gpu < gpu_count; gpu++) {
+    cudaSetDevice(gpu);
+    cudaStreamSynchronize(streams[gpu]);
+    cudaStreamDestroy(streams[gpu]);
+    cudaFree(&(dev_buffers_c[gpu]));
+    cudaFree(&(dev_buffers_b[gpu]));
+    cudaFree(&(dev_buffers_a[gpu]));
   }
 
   free(streams);
@@ -172,10 +170,12 @@ void phpc_gemm_summa_cuda(MPI_Comm grid_comm, const double *A, const double *B, 
   assert(ldb > 0);
   assert(ldc > 0);
 
-  MPI_Comm row_comm, col_comm;
-  int dims[2], periods[2], coords[2];
+  int rank, size, dims[2], periods[2], coords[2];
   int remain_dims_row[2] = {0, 1};
   int remain_dims_col[2] = {1, 0};
+  MPI_Comm row_comm, col_comm;
+  MPI_Comm_size(grid_comm, &rank);
+  MPI_Comm_size(grid_comm, &size);
   MPI_Cart_get(grid_comm, 2, dims, periods, coords);
   MPI_Cart_sub(grid_comm, remain_dims_row, &row_comm);
   MPI_Cart_sub(grid_comm, remain_dims_col, &col_comm);
@@ -194,36 +194,58 @@ void phpc_gemm_summa_cuda(MPI_Comm grid_comm, const double *A, const double *B, 
     grid_height = local_A_rows / block_width + (local_A_rows % block_width > 0);
   }
 
+  /* apply offset */
+  A += coords[0] * lda * local_A_rows + coords[1] * panel_K_dim;
+  B += coords[0] * ldb * panel_K_dim + coords[1] * local_B_cols;
+  double *offset_c = C + coords[0] * ldc * local_A_rows + coords[1] * local_B_cols;
+
   double *buffer_a = (double *)malloc(local_A_rows * panel_K_dim * sizeof(double));
   double *buffer_b = (double *)malloc(panel_K_dim * local_B_cols * sizeof(double));
 
   assert(buffer_a != NULL);
   assert(buffer_b != NULL);
 
+  MPI_Datatype block_a_type, block_b_type, block_c_type;
+  MPI_Type_vector(local_A_rows, panel_K_dim, lda, MPI_DOUBLE, &block_a_type);
+  MPI_Type_vector(panel_K_dim, local_B_cols, ldb, MPI_DOUBLE, &block_b_type);
+  MPI_Type_vector(local_A_rows, local_B_cols, ldc, MPI_DOUBLE, &block_c_type);
+  MPI_Type_commit(&block_a_type);
+  MPI_Type_commit(&block_b_type);
+  MPI_Type_commit(&block_c_type);
+
   for (int k = 0; k < lcm; k++) {
     int sender_column = k % dims[1];
     int sender_row = k % dims[0];
+    int block_lda = panel_K_dim;
+    int block_ldb = local_B_cols;
 
     const double *block_a, *block_b;
 
     if (coords[1] == sender_column) {
-      block_a = A;                     /* we are sending the block */
-      A += local_A_rows * panel_K_dim; /* we may have to send again in the future, skip the pointer to the start of the other block */
+      block_a = A;                /* we are sending the block */
+      A += dims[1] * panel_K_dim; /* we may have to send again in the future, skip the pointer to the start of the other block */
+
+      MPI_Bcast((void *)block_a, 1, block_a_type, sender_column, row_comm);
+      block_lda = lda;
     } else {
       block_a = buffer_a; /* we are receiving, prepare the buffer */
+      MPI_Bcast((void *)block_a, local_A_rows * panel_K_dim, MPI_DOUBLE, sender_column, row_comm);
     }
 
     if (coords[0] == sender_row) {
-      block_b = B;                     /* we are sending the block */
-      B += panel_K_dim * local_B_cols; /* we may have to send again in the future, skip the pointer to the start of the other block */
+      block_b = B;                      /* we are sending the block */
+      B += dims[0] * panel_K_dim * ldb; /* we may have to send again in the future, skip the pointer to the start of the other block */
+
+      MPI_Bcast((void *)block_b, 1, block_b_type, sender_row, col_comm);
+      block_ldb = ldb;
     } else {
       block_b = buffer_b; /* we are receiving, prepare the buffer */
+      MPI_Bcast((void *)block_b, panel_K_dim * local_B_cols, MPI_DOUBLE, sender_row, col_comm);
     }
 
-    MPI_Bcast((void *)block_a, local_A_rows * panel_K_dim, MPI_DOUBLE, sender_column, row_comm);
-    MPI_Bcast((void *)block_b, panel_K_dim * local_B_cols, MPI_DOUBLE, sender_row, col_comm);
+    phpc_gemm_sequential(block_a, block_lda, block_b, block_ldb, offset_c, ldc, local_A_rows, panel_K_dim, local_B_cols);
 
-    phpc_gemm_cuda(block_a, lda, block_b, ldb, C, ldc, matrices_width, gpu_count, grid_width, grid_height, block_width);
+    // phpc_gemm_cuda(block_a, block_lda, block_b, block_ldb, C, ldc, local_A_rows, panel_K_dim, local_B_cols, gpu_count, grid_width, grid_height, block_width);
   }
 
   // TODO: gather matrices
@@ -231,6 +253,11 @@ void phpc_gemm_summa_cuda(MPI_Comm grid_comm, const double *A, const double *B, 
   free(buffer_b);
   free(buffer_a);
 
+  MPI_Type_free(&block_c_type);
+  MPI_Type_free(&block_b_type);
+  MPI_Type_free(&block_a_type);
   MPI_Comm_free(&row_comm);
   MPI_Comm_free(&col_comm);
 }
+
+#undef IDX
