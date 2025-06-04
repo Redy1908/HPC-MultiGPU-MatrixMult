db4258a73efb1f1d3fd58addcd5ba745721dbb05
Redy1908 <58226371+Redy1908@users.noreply.github.com>
2025-06-04 15:27:28 +0200

    Updated

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+MPI_INCLUDE_PATH="/usr/mpi/gcc/openmpi-4.1.0rc5/include"
+MPI_LIB_PATH="/usr/mpi/gcc/openmpi-4.1.0rc5/lib64"
+
 nvcc src/main.cu src/utils.cu src/phpc_matrix_operations.cu -o bin/main_matmul.out \\
     -I"\$MPI_INCLUDE_PATH" -L"\$MPI_LIB_PATH" -Isrc \\
     -lcudart -lmpi -lcublas -lm -arch=sm_70 -lineinfo
@@ -16,9 +19,6 @@ for NPROCS in "${PROCESS_COUNTS[@]}"; do
 #SBATCH --output=logs/output_${NPROCS}process.log
 #SBATCH --error=logs/error_${NPROCS}process.log
 #SBATCH --job-name=matmul_${NPROCS}process
-
-MPI_INCLUDE_PATH="/usr/mpi/gcc/openmpi-4.1.0rc5/include"
-MPI_LIB_PATH="/usr/mpi/gcc/openmpi-4.1.0rc5/lib64"
 
 srun --mpi=pmix_v3 bash -c '
     export CUDA_VISIBLE_DEVICES=\$SLURM_LOCALID
