3b8578a61ea99c74f1825dad649fb4cb720581a8
Redy1908 <58226371+Redy1908@users.noreply.github.com>
2025-06-04 13:26:50 +0200

    Updated

--- README.md
+++ README.md
@@ -5,12 +5,15 @@
 Dalla directory root del progetto lanciare il job sul cluster:
 
 ```bash
-sbatch ./scripts/job.slurm
+chmod +x ./scripts/run.sh
+./scripts/run.sh
 ```
 
 I file di log verranno creati nella directory `logs/`:
 -   Output standard: `logs/output.log`
 -   Errori: `logs/error.log`
+
+I file CSV saranno generati nella directory `logs/`.
 
 I file di profiling generati da NVIDIA Nsight Systems saranno salvati nella directory `profiling/`:
 -   1 file per ogni processo MPI
---
+++ scripts/run.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+PROCESS_COUNTS=(1 4 16 64)
+for NPROCS in "${PROCESS_COUNTS[@]}"; do
+
+    cat > temp_job_${NPROCS}.slurm << EOF
+#!/bin/bash
+#SBATCH -p gpus
+#SBATCH --ntasks=${NPROCS}
+#SBATCH --gpus-per-task=1
+#SBATCH --time=00:10:00
+#SBATCH --output=logs/output_${NPROCS}procs_%j.log
+#SBATCH --error=logs/error_${NPROCS}procs_%j.log
+#SBATCH --job-name=matmul_${NPROCS}p
+
+MPI_INCLUDE_PATH="/usr/mpi/gcc/openmpi-4.1.0rc5/include"
+MPI_LIB_PATH="/usr/mpi/gcc/openmpi-4.1.0rc5/lib64"
+
+nvcc src/main.cu src/utils.cu src/phpc_matrix_operations.cu -o bin/main_matmul_${NPROCS}.out \\
+    -I"\$MPI_INCLUDE_PATH" -L"\$MPI_LIB_PATH" -Isrc \\
+    -lcudart -lmpi -lcublas -lm -arch=sm_70 -lineinfo
+
+srun --mpi=pmix_v3 bash -c '
+    export CUDA_VISIBLE_DEVICES=\$SLURM_LOCALID
+    bin/main_matmul_${NPROCS}.out
+'
+EOF
+
+    sbatch temp_job_${NPROCS}.slurm
+    
+    rm temp_job_${NPROCS}.slurm
+done
