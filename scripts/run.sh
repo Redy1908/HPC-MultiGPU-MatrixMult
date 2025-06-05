81ff572cd69a919f8b352a0cdbd52157efd33935
Redy1908 <58226371+Redy1908@users.noreply.github.com>
2025-06-05 11:22:47 +0200

    Updated run.sh

--- scripts/run.sh
+++ scripts/run.sh
@@ -11,26 +11,26 @@ nvcc src/main.cu src/utils.cu src/phpc_matrix_operations.cu -o bin/main_matmul.o
     -I"$MPI_INCLUDE_PATH" -L"$MPI_LIB_PATH" -Isrc \
     -lcudart -lmpi -lcublas -lm -arch=sm_70 -lineinfo
 
-PROCESS_COUNTS=(1 4 16)
+TASK_COUNTS=(1 4 16)
 
-for NPROCS in "${PROCESS_COUNTS[@]}"; do
+for NTASK in "${TASK_COUNTS[@]}"; do
 
-    cat > temp_job_${NPROCS}.slurm << EOF
+    cat > temp_job_${NTASK}.slurm << EOF
 #!/bin/bash
 #SBATCH -p gpus
-#SBATCH --ntasks=${NPROCS}
+#SBATCH --ntasks=${NTASK}
 #SBATCH --ntasks-per-node=1
-#SBATCH --cpus-per-task=1
 #SBATCH --gpus-per-task=1
+#SBATCH --cpus-per-task=1
 #SBATCH --time=00:10:00
-#SBATCH --output=logs/output_${NPROCS}process.log
-#SBATCH --error=logs/error_${NPROCS}process.log
-#SBATCH --job-name=matmul_${NPROCS}process
+#SBATCH --output=logs/output_${NTASK}process.log
+#SBATCH --error=logs/error_${NTASK}process.log
+#SBATCH --job-name=matmul_${NTASK}process
 
 srun bin/main_matmul.out
 EOF
 
-    sbatch temp_job_${NPROCS}.slurm
+    sbatch temp_job_${NTASK}.slurm
     
-    rm temp_job_${NPROCS}.slurm
+    rm temp_job_${NTASK}.slurm
 done
