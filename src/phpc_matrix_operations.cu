4f1daf3547424f0553e7595c5ee2f31fed90aa6e
Salvatore Di Gennaro <salvatore.digennaro@outlook.it>
2025-06-09 21:00:55 +0200

    Fix cuBLAS handles

--- src/phpc_matrix_operations.cu
+++ src/phpc_matrix_operations.cu
@@ -106,8 +106,10 @@ int phpc_gemm_cublas(const double *a, int lda, const double *b, int ldb, double 
   double **dev_buffers_b = (double **)malloc(gpu_count * sizeof(double *));
   double **dev_buffers_c = (double **)malloc(gpu_count * sizeof(double *));
   cudaStream_t *streams = (cudaStream_t *)malloc(gpu_count * sizeof(cudaStream_t));
+  cublasHandle_t *handles = (cublasHandle_t *)malloc(gpu_count * sizeof(cublasHandle_t));
 
-  if (dev_buffers_a == NULL || dev_buffers_b == NULL || dev_buffers_c == NULL || streams == NULL) {
+  if (dev_buffers_a == NULL || dev_buffers_b == NULL || dev_buffers_c == NULL || streams == NULL || handles == NULL) {
+    free(handles);
     free(streams);
     free(dev_buffers_c);
     free(dev_buffers_b);
@@ -115,72 +117,70 @@ int phpc_gemm_cublas(const double *a, int lda, const double *b, int ldb, double 
     return 2;
   }
 
-  cublasHandle_t handle;
-  if (cublasCreate(&handle) != CUBLAS_STATUS_SUCCESS) {
-    return_code = 3;
-  } else {
-    int launched_kernels = 0;
+  int launched_kernels = 0;
 
-    for (int gpu = 0; gpu < gpu_count; gpu++) {
-      cudaSetDevice(gpu);
-      cudaError_t status;
+  for (int gpu = 0; gpu < gpu_count; gpu++) {
+    cudaSetDevice(gpu);
+    cublasCreate_v2(&(handles[gpu]));
 
-      status = cudaStreamCreate(&(streams[gpu]));
-      if (status != cudaSuccess) {
-        return_code = 1;
-        break;
-      }
-      launched_kernels++;
+    cudaError_t status;
 
-      status = cudaMallocAsync(&(dev_buffers_a[gpu]), m * k * sizeof(double), streams[gpu]);
-      if (status != cudaSuccess) {
-        return_code = 1;
-        break;
-      }
+    status = cudaStreamCreate(&(streams[gpu]));
+    if (status != cudaSuccess) {
+      return_code = 1;
+      break;
+    }
+    launched_kernels++;
 
-      status = cudaMallocAsync(&(dev_buffers_b[gpu]), k * dev_n * sizeof(double), streams[gpu]);
-      if (status != cudaSuccess) {
-        cudaFreeAsync(dev_buffers_a[gpu], streams[gpu]);
-        return_code = 1;
-        break;
-      }
+    status = cudaMallocAsync(&(dev_buffers_a[gpu]), m * k * sizeof(double), streams[gpu]);
+    if (status != cudaSuccess) {
+      return_code = 1;
+      break;
+    }
 
-      status = cudaMallocAsync(&(dev_buffers_c[gpu]), m * dev_n * sizeof(double), streams[gpu]);
-      if (status != cudaSuccess) {
-        cudaFreeAsync(dev_buffers_b[gpu], streams[gpu]);
-        cudaFreeAsync(dev_buffers_a[gpu], streams[gpu]);
-        return_code = 1;
-        break;
-      }
+    status = cudaMallocAsync(&(dev_buffers_b[gpu]), k * dev_n * sizeof(double), streams[gpu]);
+    if (status != cudaSuccess) {
+      cudaFreeAsync(dev_buffers_a[gpu], streams[gpu]);
+      return_code = 1;
+      break;
+    }
 
-      /* copy from host to device */
-      cudaMemcpy2DAsync(dev_buffers_a[gpu], k * sizeof(double), a, lda * sizeof(double), k * sizeof(double), m, cudaMemcpyHostToDevice, streams[gpu]);
-      cudaMemcpy2DAsync(dev_buffers_b[gpu], dev_n * sizeof(double), b + dev_n * gpu, ldb * sizeof(double), dev_n * sizeof(double), k, cudaMemcpyHostToDevice, streams[gpu]);
-      cudaMemcpy2DAsync(dev_buffers_c[gpu], dev_n * sizeof(double), c + dev_n * gpu, ldc * sizeof(double), dev_n * sizeof(double), m, cudaMemcpyHostToDevice, streams[gpu]);
-
-      /* perform computation */
-      /* note: some subtle math magic to make it work since cublas expects column-major matrices https://stackoverflow.com/a/56064726/17731255 */
-      double alpha = 1, beta = 0;
-      cublasSetStream_v2(handle, streams[gpu]);
-      cublasDgemm(handle, CUBLAS_OP_N, CUBLAS_OP_N, dev_n, m, k, &alpha, dev_buffers_b[gpu], n, dev_buffers_a[gpu], k, &beta, dev_buffers_c[gpu], n);
-
-      /* copy result from device to host */
-      cudaMemcpy2DAsync(c + dev_n * gpu, ldc * sizeof(double), dev_buffers_c[gpu], dev_n * sizeof(double), dev_n * sizeof(double), m, cudaMemcpyDeviceToHost, streams[gpu]);
-
-      cudaFreeAsync(dev_buffers_c[gpu], streams[gpu]);
+    status = cudaMallocAsync(&(dev_buffers_c[gpu]), m * dev_n * sizeof(double), streams[gpu]);
+    if (status != cudaSuccess) {
       cudaFreeAsync(dev_buffers_b[gpu], streams[gpu]);
       cudaFreeAsync(dev_buffers_a[gpu], streams[gpu]);
+      return_code = 1;
+      break;
     }
 
-    for (int gpu = 0; gpu < launched_kernels; gpu++) {
-      cudaSetDevice(gpu);
-      cudaStreamSynchronize(streams[gpu]);
-      cudaStreamDestroy(streams[gpu]);
-    }
+    /* copy from host to device */
+    // cublasSetMatrix(k, m, sizeof(double), a, lda, dev_buffers_a[gpu], k);
+    cudaMemcpy2DAsync(dev_buffers_a[gpu], k * sizeof(double), a, lda * sizeof(double), k * sizeof(double), m, cudaMemcpyHostToDevice, streams[gpu]);
+    cudaMemcpy2DAsync(dev_buffers_b[gpu], dev_n * sizeof(double), b + dev_n * gpu, ldb * sizeof(double), dev_n * sizeof(double), k, cudaMemcpyHostToDevice, streams[gpu]);
+    cudaMemcpy2DAsync(dev_buffers_c[gpu], dev_n * sizeof(double), c + dev_n * gpu, ldc * sizeof(double), dev_n * sizeof(double), m, cudaMemcpyHostToDevice, streams[gpu]);
 
-    cublasDestroy(handle);
+    /* perform computation */
+    /* note: some subtle math magic to make it work since cublas expects column-major matrices https://stackoverflow.com/a/56064726/17731255 */
+    double alpha = 1, beta = 0;
+    cublasSetStream_v2(handle, streams[gpu]);
+    cublasDgemm_v2(handle, CUBLAS_OP_N, CUBLAS_OP_N, dev_n, m, k, &alpha, dev_buffers_b[gpu], n, dev_buffers_a[gpu], k, &beta, dev_buffers_c[gpu], n);
+
+    /* copy result from device to host */
+    cudaMemcpy2DAsync(c + dev_n * gpu, ldc * sizeof(double), dev_buffers_c[gpu], dev_n * sizeof(double), dev_n * sizeof(double), m, cudaMemcpyDeviceToHost, streams[gpu]);
+
+    cudaFreeAsync(dev_buffers_c[gpu], streams[gpu]);
+    cudaFreeAsync(dev_buffers_b[gpu], streams[gpu]);
+    cudaFreeAsync(dev_buffers_a[gpu], streams[gpu]);
   }
 
+  for (int gpu = 0; gpu < launched_kernels; gpu++) {
+    cudaSetDevice(gpu);
+    cudaStreamSynchronize(streams[gpu]);
+    cudaStreamDestroy(streams[gpu]);
+    cublasDestroy_v2(handles[gpu]);
+  }
+
+  free(handles);
   free(streams);
   free(dev_buffers_c);
   free(dev_buffers_b);
