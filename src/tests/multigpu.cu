98ea3ef520d6bce9ef124d0ed2e2853c50be0326
Salvatore Di Gennaro <salvatore.digennaro@outlook.it>
2025-06-11 10:40:36 +0200

    testing

--- src/tests/multigpu.cu
+++ src/tests/multigpu.cu
@@ -1,26 +1,33 @@
 #include <assert.h>
+#include <cuda_runtime.h>
 #include <mpi.h>
 #include <stdio.h>
 #include <stdlib.h>
 
 #include "../phpc_matrix_operations.cuh"
 
-int main(int argc, char **argv) {
-  assert(argc >= 5);
+#define MPI_ASSERT(condition)                  \
+  {                                            \
+    if (!(condition)) {                        \
+      MPI_Abort(MPI_COMM_WORLD, EXIT_FAILURE); \
+    }                                          \
+  }
 
+int main(int argc, char **argv) {
   MPI_Init(&argc, &argv);
 
   MPI_Comm grid_comm;
   int dims[2], period[2], rank;
 
   int width = atoi(argv[1]);
-  int gpus = atoi(argv[2]);
-  dims[0] = atoi(argv[3]);
-  dims[1] = atoi(argv[4]);
+  dims[0] = atoi(argv[2]);
+  dims[1] = atoi(argv[3]);
   period[0] = period[1] = 1;
 
-  assert(gpus > 0);
-  assert(width >= gpus);
+  int gpus;
+  cudaGetDeviceCount(&gpus);
+  MPI_ASSERT(gpus > 0);
+  MPI_ASSERT(width >= gpus);
 
   MPI_Cart_create(MPI_COMM_WORLD, 2, dims, period, 0, &grid_comm);
   MPI_Comm_rank(MPI_COMM_WORLD, &rank);
@@ -29,7 +36,7 @@ int main(int argc, char **argv) {
   double *b = (double *)malloc(width * width * sizeof(double));
   double *c = (double *)malloc(width * width * sizeof(double));
 
-  assert(a != NULL && b != NULL && c != NULL);
+  MPI_ASSERT(a != NULL && b != NULL && c != NULL);
 
   for (size_t i = 0; i < width * width; i++) {
     a[i] = rank + 1;
@@ -37,7 +44,7 @@ int main(int argc, char **argv) {
   }
 
   int status = phpc_gemm_summa_cublas(grid_comm, a, b, c, width, gpus, 1, 1, 32);
-  assert(status == 0);
+  MPI_ASSERT(status == 0);
 
   if (rank == 0) {
     for (size_t i = 0; i < width; i++) {
