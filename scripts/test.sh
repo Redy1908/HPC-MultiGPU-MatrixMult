19f9f8ccfb38fb5952d126f974b7b04a71719a7b
Salvatore Di Gennaro <salvatore.digennaro@outlook.it>
2025-06-19 14:36:06 +0200

    Test

--- scripts/test.sh
+++ scripts/test.sh
@@ -5,6 +5,4 @@ mkdir -p bin
 MPI_INCLUDE_PATH="/usr/mpi/gcc/openmpi-4.1.0rc5/include"
 MPI_LIB_PATH="/usr/mpi/gcc/openmpi-4.1.0rc5/lib64"
 
-nvcc src/main.cu src/utils.cu src/phpc_matrix_operations.cu -o bin/main.out \
-    -I"$MPI_INCLUDE_PATH" -L"$MPI_LIB_PATH" -Isrc \
-    -lcudart -lmpi -lcublas -lm -arch=sm_70 -lineinfo && sbatch $1
+nvcc src/main.cu -o bin/main.out -I"$MPI_INCLUDE_PATH" -L"$MPI_LIB_PATH" -lmpi && sbatch $1
