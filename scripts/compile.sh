eb77863b73a155972df4a364f3a0252958dd6afd
Salvatore Di Gennaro <salvatore.digennaro@outlook.it>
2025-06-19 15:02:41 +0200

    Test

--- scripts/compile.sh
+++ scripts/compile.sh
@@ -2,7 +2,7 @@
 
 mkdir -p bin
 
-MPI_INCLUDE_PATH="/usr/mpi/gcc/openmpi-4.1.0rc5/include"
-MPI_LIB_PATH="/usr/mpi/gcc/openmpi-4.1.0rc5/lib64"
+# MPI_INCLUDE_PATH="/usr/mpi/gcc/openmpi-4.1.0rc5/include"
+# MPI_LIB_PATH="/usr/mpi/gcc/openmpi-4.1.0rc5/lib64"
 
-nvcc src/main.cu -o bin/main.out -I"$MPI_INCLUDE_PATH" -L"$MPI_LIB_PATH" -lmpi
+nvcc src/main.cu -o bin/main.out -I/usr/mpi/gcc/openmpi-4.1.0rc5/include -pthread -L/usr/lib64 -Wl,-rpath -Wl,/usr/lib64 -Wl,-rpath -Wl,/usr/mpi/gcc/openmpi-4.1.0rc5/lib64 -Wl,--enable-new-dtags -L/usr/mpi/gcc/openmpi-4.1.0rc5/lib64 -lmpi
