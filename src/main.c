efd0c49cef020a982935ac1030defc66bb459866
Salvatore Di Gennaro <salvatore.digennaro@outlook.it>
2025-06-25 12:16:07 +0200

    Test

--- src/main.c
+++ src/main.c
@@ -1,5 +1,5 @@
 #include <math.h>
-#include <mpi/mpi.h>
+#include <mpi.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
--- src/phpc_matrix_operations.h
+++ src/phpc_matrix_operations.h
@@ -1,7 +1,7 @@
 #ifndef _PHPC_MATRIX_OPERATIONS_H
 #define _PHPC_MATRIX_OPERATIONS_H
 
-#include <mpi/mpi.h>
+#include <mpi.h>
 
 void phpc_gemm_iterative(const double *A, const double *B, double *C, int N);
 
