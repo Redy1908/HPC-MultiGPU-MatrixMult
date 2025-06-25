70b1e18ff4f48c88cbed11b919e70a2f32b37831
Salvatore Di Gennaro <salvatore.digennaro@outlook.it>
2025-06-25 12:29:48 +0200

    Test

--- scripts/run.sh
+++ scripts/run.sh
@@ -68,9 +68,9 @@ for csv_file_path in "$CSV_FILES_DIR"/*.csv; do
 #SBATCH --time=00:10:00
 #SBATCH --output=logs/output_${JOB_NAME_SUFFIX}.log
 #SBATCH --error=logs/error_${JOB_NAME_SUFFIX}.err
-#SBATCH --job-name=mat_mul
+#SBATCH --job-name=test
 
-srun bin/main.out ${MSIZE} ${TILE_WIDTH} ${GRID_WIDTH} ${GRID_HEIGHT} ${csv_filename_no_ext}
+srun bin/test ${MSIZE} ${TILE_WIDTH} ${GRID_WIDTH} ${GRID_HEIGHT} ${csv_filename_no_ext}
 EOF
             JOB_OUTPUT=$(sbatch ${SLURM_SCRIPT_NAME_TMP})
             JOB_ID=$(echo "$JOB_OUTPUT" | grep -o '[0-9]*$')
---
+++ src/test.c
@@ -0,0 +1,24 @@
+#include <mpi.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char **argv) {
+  MPI_Init(&argc, &argv);
+
+  int rank, size;
+  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+  MPI_Comm_size(MPI_COMM_WORLD, &size);
+
+  MPI_Comm grid_comm;
+  int dims[2] = {0, 0}, periods[2] = {0, 0};
+
+  MPI_Dims_create(size, 2, dims);
+  MPI_Cart_create(MPI_COMM_WORLD, 2, dims, periods, 0, &grid_comm);
+
+  if (rank == 0)
+    printf("Grid: %d %d\n", dims[0], dims[1]);
+
+  MPI_Finalize();
+
+  return 0;
+}
