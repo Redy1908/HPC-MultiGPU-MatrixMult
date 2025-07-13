c6c8f53a92a41295761abcd603cdc70d0b795393
Salvatore Di Gennaro <salvatore.digennaro@outlook.it>
2025-07-13 17:03:07 +0200

    Tests

---
+++ scripts/tests.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+run() {
+    filename="$6_N$2_T$1_G1_TW$3_GW$4_GH$5"
+    mpirun -n $1 bin/main.out $2 $3 $4 $5 $filename
+}
+
+run 1 128 32 1 1 testA
+run 2 128 32 1 1 testA
+run 4 128 32 1 1 testA
+run 8 128 32 1 1 testA
+
+run 4 512 4 1 1 testA2
+run 4 512 8 1 1 testA2
+run 4 512 16 1 1 testA2
+run 4 512 32 1 1 testA2
+run 4 512 32 2 2 testA2
+run 4 512 32 4 4 testA2
+
+run 1 32 32 1 1 testB
+run 4 64 32 1 1 testB
+
+run 4 8 4 1 1 testC
+run 4 16 8 1 1 testC
+run 4 32 16 1 1 testC
+run 4 64 32 1 1 testC
+run 4 128 32 2 2 testC
+run 4 256 32 4 4 testC
+
+run 1 64 32 2 2 testC
+run 4 128 32 2 2 testC
+
+# echo "matrix_size,n_proc,n_gpu,n_block,n_thread_per_block,method,time" >> csv/testA2.csv
+# cat csv/testA2_*.csv | sort -h >> csv/testA2.csv
+# rm csv/testA2_*.csv
+# python3 plots.py
--- src/main.c
+++ src/main.c
@@ -74,48 +74,48 @@ int main(int argc, char *argv[]) {
   // Test di correttezza
   // ==================================================
   // FIXME: don't need to run this test every time once we estabilished it works
-  // if (rank == 0) {
-  //   printf("\nInitializing matrix A and B to 2.0, matrix C to 0.0...\n");
-  // }
-  // for (i = 0; i < N; i++) {
-  //   for (j = 0; j < N; j++) {
-  //     *(A + i * N + j) = 2.0;
-  //     *(B + i * N + j) = 2.0;
-  //     *(C + i * N + j) = 0.0;
-  //   }
-  // }
-  // if (rank == 0) {
-  //   printf("Matrix initialization complete.\n");
-  //   printf("\nRunning correctness test...\n");
-  // }
+  if (rank == 0) {
+    printf("\nInitializing matrix A and B to 2.0, matrix C to 0.0...\n");
+  }
+  for (i = 0; i < N; i++) {
+    for (j = 0; j < N; j++) {
+      *(A + i * N + j) = 2.0;
+      *(B + i * N + j) = 2.0;
+      *(C + i * N + j) = 0.0;
+    }
+  }
+  if (rank == 0) {
+    printf("Matrix initialization complete.\n");
+    printf("\nRunning correctness test...\n");
+  }
 
-  // phpc_gemm_summa_cuda(grid_comm, A, B, C, N, gpu_count, grid_width, grid_height, tile_width);
+  phpc_gemm_summa_cuda(grid_comm, A, B, C, N, gpu_count, grid_width, grid_height, tile_width);
 
-  // if (rank == 0) {
-  //   printf("Checking correctness...\n");
-  //   int test_correctness = 1;
-  //   double expected_value = 4.0 * N;
-  //   double epsilon = 1e-9;
+  if (rank == 0) {
+    printf("Checking correctness...\n");
+    int test_correctness = 1;
+    double expected_value = 4.0 * N;
+    double epsilon = 1e-9;
 
-  //   for (i = 0; i < N; i++) {
-  //     for (j = 0; j < N; j++) {
-  //       if (fabs(C[i * N + j] - expected_value) > epsilon) {
-  //         fprintf(stderr, "Correcteness error at rank %d, C[%d][%d] = %f, expected %f\n", rank, i, j, C[i * N + j], expected_value);
-  //         test_correctness = 0;
-  //       }
-  //     }
-  //   }
+    for (i = 0; i < N; i++) {
+      for (j = 0; j < N; j++) {
+        if (fabs(C[i * N + j] - expected_value) > epsilon) {
+          fprintf(stderr, "Correcteness error at rank %d, C[%d][%d] = %f, expected %f\n", rank, i, j, C[i * N + j], expected_value);
+          test_correctness = 0;
+        }
+      }
+    }
 
-  //   if (test_correctness) {
-  //     printf("Correctness test passed.\n");
-  //   } else {
-  //     printf("Correctness test FAILED. Aborting...\n");
-  //     free(A);
-  //     free(B);
-  //     free(C);
-  //     MPI_Abort(MPI_COMM_WORLD, EXIT_FAILURE);
-  //   }
-  // }
+    if (test_correctness) {
+      printf("Correctness test passed.\n");
+    } else {
+      printf("Correctness test FAILED. Aborting...\n");
+      free(A);
+      free(B);
+      free(C);
+      MPI_Abort(MPI_COMM_WORLD, EXIT_FAILURE);
+    }
+  }
 
   // ==================================================
   // Test di efficienza
