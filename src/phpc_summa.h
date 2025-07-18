792bc13bc5de0b3ebf6771de3acdb251129645b6
Salvatore Di Gennaro <salvatore.digennaro@outlook.it>
2025-07-18 11:52:50 +0200

    Header fix

--- src/phpc_summa.h
+++ src/phpc_summa.h
@@ -1,7 +1,7 @@
 #ifndef _PHPC_SUMMA_H
 #define _PHPC_SUMMA_H
 
-#include <mpi/mpi.h>
+#include <mpi.h>
 
 void phpc_gemm_summa_cuda(MPI_Comm grid_comm, const double *A, const double *B, double *C, int n, int gpu_count, int grid_width, int grid_height, int block_width, float *compute_time);
 
